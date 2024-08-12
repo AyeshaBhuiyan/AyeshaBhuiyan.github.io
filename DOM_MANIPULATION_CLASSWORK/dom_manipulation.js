a5dc1818e8202acb4b36b0077cae56daea7fd1a5
Ayesha Bhuiyan <abhuiyan122@gmail.com>
2024-08-12 11:51:17 +0000

    second page done

--- DOM_MANIPULATION_CLASSWORK/dom_manipulation.js
+++
@@ -1,7 +0,0 @@
-let styleButton = document.querySelector('#random-style')
-function randomStyle(){
-    document.body.style.fontFamily = "Garamond"
-    document.body.style.fontSize = "20px"
-    document.body.style.color = "purple"
-}
-styleButton.addEventListener("click", randomStyle)
